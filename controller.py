779b4bb87dbceab5cef93afd0fe23af33ccd361d
JPalakapilly <jjp126@gmail.com>
2019-10-18 15:29:33 -0700

    Made a deterministic, binning controller

--- controller.py
+++ controller.py
@@ -28,18 +28,33 @@ class BaseController():
         pass
 
 
-class RLController(BaseController):
+class BinController(BaseController):
     """
 
     """
-    def __init__(self):
-        self.policy = SimpleNet()
+    def __init__(self, action_dimension=3, scaling_factor=1, vertical_shift=10):
+		self.ac_dim = action_dimension
+		self.shift = vertical_shift
+		self.scale = scaling_factor
+
+	def _process_price_signal(self, price_signal):
+		max_price = np.max(price_signal)
+		min_price = np.min(price_signal)
+		bins = np.linspace(min_price, max_price, self.ac_dim)
+		binned_state = np.digitize(price_signal, bins) - np.ones(price_signal)
+		return binned_state
 
     def get_points(price_signal):
-        return self.policy(price_signal)
+		post_process = self._process_price_signal(price_signal)
+        return self.policy(post_process)
+
+	def policy(state):
+		return self.scale * state + vertical_shift
 
     def update(reward):
         pass
+
+
 
 
 
