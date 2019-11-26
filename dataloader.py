568e711d2a52a2e442c0b609cb085509d5fe549a
akashgokul <akashgokul@berkeley.edu>
2019-11-25 21:39:32 -0800

    Fixing time conversion for timestamp

--- dataloader.py
+++ dataloader.py
@@ -4,10 +4,6 @@ import pytz
 def convert_times(df):
     df['TimeStamp'] = pd.to_datetime(df['TimeStamp'])
     df['TimeStamp'] = df['TimeStamp'].dt.tz_localize(pytz.timezone('Singapore'),ambiguous='infer')
-    df['TimeStamp (ST)'] = df['TimeStamp'].dt.tz_convert(pytz.timezone('America/Los_Angeles'))
-    df['TimeStamp (ST)'] = pd.to_datetime(df['TimeStamp (ST)']) - pd.Timedelta(1, unit='h')
-    df['Day (ST)'] = df['TimeStamp (ST)'].dt.date
-    df['Hour (ST)'] = df['TimeStamp (ST)'].dt.hour
-    df = df.sort_values(by=['Day (ST)', 'Hour (ST)'])
-
+    df['TimeStamp'] = df['TimeStamp'].dt.tz_convert(pytz.timezone('America/los_angeles'))
+    df['TimeStamp'] = pd.to_datetime(df['TimeStamp']) - pd.Timedelta(1, unit='h')
     return df
