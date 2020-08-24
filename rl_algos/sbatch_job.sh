9e214c70060fdcbcd3d82de5034cfd3b96164a09
akashgokul <akashgokul@berkeley.edu>
2020-08-24 13:45:04 -0400

    Updating error in sbatch script

--- rl_algos/sbatch_job.sh
+++ rl_algos/sbatch_job.sh
@@ -27,5 +27,6 @@
 #SBATCH --mail-user=akashgokul@berkeley.edu
 ## Command(s) to run (example):
 module load python/3.6
+module load tensorflow/1.12.0-py36-pip-gpu
 source activate /global/scratch/akashgokul/conda/test
 python3 StableBaselines.py sac
