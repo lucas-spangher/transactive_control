47064f7e6356a3f982b44fbc3f4779c8e5cdb6d4
Manan Khattar <khattar.manan@gmail.com>
2020-10-16 15:24:57 -0700

    after formatting

--- gym-socialgame/gym_socialgame/envs/socialgame_env_planning.py
+++ gym-socialgame/gym_socialgame/envs/socialgame_env_planning.py
@@ -12,29 +12,34 @@ import tensorflow as tf
 
 import pickle
 
-class SocialGamePlanningEnv(SocialGameEnv):
-    def __init__(self, 
-        action_space_string = "continuous", 
-        response_type_string = "l", 
-        number_of_participants = 10,
-        one_day = 0, 
-        energy_in_state = False, 
-        yesterday_in_state = False,
-        day_of_week = False,
-        pricing_type= "TOU",
-        planning_flag = False,
-        planning_steps = 0,
-        planning_model_type = "Oracle",
-        own_tb_log = None):
 
-        super().__init__(action_space_string, 
-        response_type_string, 
-        number_of_participants,
-        one_day, 
-        energy_in_state, 
-        yesterday_in_state,
-        day_of_week,
-        pricing_type,
+class SocialGamePlanningEnv(SocialGameEnv):
+    def __init__(
+        self,
+        action_space_string="continuous",
+        response_type_string="l",
+        number_of_participants=10,
+        one_day=0,
+        energy_in_state=False,
+        yesterday_in_state=False,
+        day_of_week=False,
+        pricing_type="TOU",
+        planning_flag=False,
+        planning_steps=0,
+        planning_model_type="Oracle",
+        own_tb_log=None,
+        reward_function="scaled_cost_distance",
+    ):
+
+        super().__init__(
+            action_space_string,
+            response_type_string,
+            number_of_participants,
+            one_day,
+            energy_in_state,
+            yesterday_in_state,
+            day_of_week,
+            pricing_type,
         )
 
         self.planning_flag = planning_flag
@@ -42,14 +47,9 @@ class SocialGamePlanningEnv(SocialGameEnv):
         self.planning_model_type = planning_model_type
 
     def _planning_prediction(
-        self,
-        action, 
-        day_of_week, 
-        planning_model_type = "OLS", 
-        loaded_model = None,
+        self, action, day_of_week, planning_model_type="OLS", loaded_model=None,
     ):
-        
-        
+
         """
         Function for calling the planning model and producing an average response 
         
@@ -63,32 +63,31 @@ class SocialGamePlanningEnv(SocialGameEnv):
         loaded_model: pass in a loaded model
 
         """
-        
-        # if self.min_demand is not None and self.max_demand is not None: 
+
+        # if self.min_demand is not None and self.max_demand is not None:
         #     scaler = MinMaxScaler(feature_range = (self.min_demand, self.max_demand))
 
         energy_consumptions = {}
-        total_consumption  = np.zeros(10)
+        total_consumption = np.zeros(10)
 
         if planning_model_type == "Oracle":
             prev_observation = self.prices[(self.day)]
             energy_consumptions = self._simulate_humans(action)
             return energy_consumptions
-            
-        
-        ## Basic LSTM that follows the rules of the experiment    
+
+        ## Basic LSTM that follows the rules of the experiment
         elif planning_model_type == "LSTM":
             ## load the minMaxScalers
-            with open ("scaler_X.pickle", "rb") as input_file:
-                scaler_X = pickle.load(input_file) 
-            with open ("scaler_y.pickle", "rb") as input_file:
-                scaler_y = pickle.load(input_file) 
+            with open("scaler_X.pickle", "rb") as input_file:
+                scaler_X = pickle.load(input_file)
+            with open("scaler_y.pickle", "rb") as input_file:
+                scaler_y = pickle.load(input_file)
 
             ## prepare the data
 
-            d_X = pd.DataFrame(data = { "action"  : action, "dow" : day_of_week } )
+            d_X = pd.DataFrame(data={"action": action, "dow": day_of_week})
             scaled_X = scaler_X.transform(d_X)
-            sxr = scaled_X.reshape((scaled_X.shape[0], 1, scaled_X.shape[1])) 
+            sxr = scaled_X.reshape((scaled_X.shape[0], 1, scaled_X.shape[1]))
             print(sxr)
 
             for player_name in self.player_dict:
@@ -101,8 +100,8 @@ class SocialGamePlanningEnv(SocialGameEnv):
 
                 preds = loaded_model.predict(sxr)
 
-                inv_preds = scaler_y.inverse_transform(preds)  
-                
+                inv_preds = scaler_y.inverse_transform(preds)
+
                 scaler = MinMaxScaler((player_min_demand, player_max_demand))
                 inv_preds = scaler.fit_transform(inv_preds.reshape(-1, 1))
 
@@ -113,9 +112,8 @@ class SocialGamePlanningEnv(SocialGameEnv):
             energy_consumptions["avg"] = total_consumption / self.number_of_participants
             # print(energy_consumptions["avg"])
             return energy_consumptions
-        
-        
-        # simple OLS trained on small dataset without IV 
+
+        # simple OLS trained on small dataset without IV
         elif planning_model_type == "OLS":
 
             for player_name in self.player_dict:
@@ -138,7 +136,7 @@ class SocialGamePlanningEnv(SocialGameEnv):
             energy_consumptions["avg"] = total_consumption / self.number_of_participants
 
             return energy_consumptions
-        
+
         # baseline model that just returns average of the sample energy day
         elif planning_model_type == "Baseline":
             for player_name in self.player_dict:
@@ -148,8 +146,9 @@ class SocialGamePlanningEnv(SocialGameEnv):
                 player_min_demand = player.get_min_demand()
                 player_max_demand = player.get_max_demand()
 
-                energy = np.repeat(70.7, len(action)) + \
-                    np.random.uniform(size = len(action))
+                energy = np.repeat(70.7, len(action)) + np.random.uniform(
+                    size=len(action)
+                )
 
                 scaler = MinMaxScaler((player_min_demand, player_max_demand))
                 energy = scaler.fit_transform(energy.reshape(-1, 1))
@@ -160,14 +159,13 @@ class SocialGamePlanningEnv(SocialGameEnv):
             energy_consumptions["avg"] = energy
 
             return energy_consumptions
-        
+
         else:
             raise ValueError("wrong planning model choice")
             return
 
-
-    def load_model_from_disk(self, file_name = "GPyOpt_planning_model"):
-        json_file = open(file_name + ".json", 'r')
+    def load_model_from_disk(self, file_name="GPyOpt_planning_model"):
+        json_file = open(file_name + ".json", "r")
         loaded_model_json = json_file.read()
         json_file.close()
         loaded_model = tf.keras.models.model_from_json(loaded_model_json)
@@ -177,7 +175,6 @@ class SocialGamePlanningEnv(SocialGameEnv):
 
         loaded_model.compile(loss="mse", optimizer="adam")
         return loaded_model
-
 
     def step(self, action, step_num=0):
         """
@@ -195,15 +192,14 @@ class SocialGamePlanningEnv(SocialGameEnv):
         Exceptions:
             raises AssertionError if action is not in the action space
         """
-        
 
-        if(not self.action_space.contains(action)):
+        if not self.action_space.contains(action):
             action = np.asarray(action)
-            if(self.action_space_string == 'continuous'):
+            if self.action_space_string == "continuous":
                 action = np.clip(action, 0, 10)
 
-            elif(self.action_space_string == 'multidiscrete'):
-                action = np.clip(action, 0, 2) 
+            elif self.action_space_string == "multidiscrete":
+                action = np.clip(action, 0, 2)
 
         prev_price = self.prices[(self.day)]
         self.day = (self.day + 1) % 365
@@ -214,10 +210,10 @@ class SocialGamePlanningEnv(SocialGameEnv):
         #     done = False
 
         points = self._points_from_action(action)
-        
+
         if self.curr_iter > 0:
             done = True
-        else: 
+        else:
             done = False
 
         energy_consumptions = self._simulate_humans(points)
@@ -228,12 +224,10 @@ class SocialGamePlanningEnv(SocialGameEnv):
         observation = self._get_observation()
         reward = self._get_reward(prev_price, energy_consumptions)
 
-
         info = {}
         return observation, reward, done, info
 
-
-    def planning_step(self, action, step_num=0): ## TODO: replace load model in SAC 
+    def planning_step(self, action, step_num=0):  ## TODO: replace load model in SAC
         """
         Purpose: Takes a step in the environment 
 
@@ -249,15 +243,14 @@ class SocialGamePlanningEnv(SocialGameEnv):
         Exceptions:
             raises AssertionError if action is not in the action space
         """
-        
 
-        if(not self.action_space.contains(action)):
+        if not self.action_space.contains(action):
             action = np.asarray(action)
-            if(self.action_space_string == 'continuous'):
+            if self.action_space_string == "continuous":
                 action = np.clip(action, 0, 10)
 
-            elif(self.action_space_string == 'multidiscrete'):
-                action = np.clip(action, 0, 2) 
+            elif self.action_space_string == "multidiscrete":
+                action = np.clip(action, 0, 2)
 
         prev_price = self.prices[(self.day)]
         self.day = (self.day + 1) % 365
@@ -268,10 +261,10 @@ class SocialGamePlanningEnv(SocialGameEnv):
         #     done = False
 
         points = self._points_from_action(action)
-        
+
         if self.curr_iter > 0:
             done = True
-        else: 
+        else:
             done = False
 
         loaded_model = None
@@ -280,11 +273,11 @@ class SocialGamePlanningEnv(SocialGameEnv):
             loaded_model = self.load_model_from_disk("GPyOpt_planning_model")
 
         energy_consumptions = self._planning_prediction(
-                        action = points, 
-                        day_of_week = self.day_of_week, 
-                        planning_model_type = self.planning_model_type, 
-                        loaded_model = loaded_model,
-                        )
+            action=points,
+            day_of_week=self.day_of_week,
+            planning_model_type=self.planning_model_type,
+            loaded_model=loaded_model,
+        )
 
         # HACK ALERT. USING AVG ENERGY CONSUMPTION FOR STATE SPACE. this will not work if people are not all the same
         self.prev_energy = energy_consumptions["avg"]
@@ -297,7 +290,4 @@ class SocialGamePlanningEnv(SocialGameEnv):
         info = {}
 
         return observation, reward, done, info
-
-
-
 
